14ee886fad4b686096b7f7ff6893791cc37790e7
Daniel P. Shannon <daniel@fleet.i.ng>
2021-10-06 05:25:09 -0400

    ✨ Start "20020."

---
+++ src/pages/20020.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from 'react'
+
+export default function TwentyThousandTwentyPage(): ReactElement {
+  return <div />
+}
---
+++ src/styles/pages/20020/index.ts
@@ -0,0 +1,5 @@
+import * as Base from './base'
+
+export {
+  Base,
+}
